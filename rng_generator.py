29fc66d20a02b874936c57d4da785b01abe50b01
Bo <ybly.1993@gmail.com>
2024-05-10 00:19:59 +0100

    Initial commit

---
+++ rng_generator.py
@@ -0,0 +1,30 @@
+import random
+
+def rng_picker(lowerLimit, upperLimit, amount):
+    min_num = min(lowerLimit, upperLimit)
+    max_num = max(lowerLimit, upperLimit)
+    if max_num - min_num + 1 < amount:
+        print("Error: The range of numbers is smaller than the amount of numbers to generate.")
+        return []
+    random_numbers = random.sample(range(min_num, max_num + 1), amount)
+    return random_numbers
+
+def main():
+    print("-------------------------------------------------------")
+    lowerLimit = int(input("--> Enter the lower limit: \t"))
+    upperLimit = int(input("--> Enter the upper limit: \t"))
+    amount = int(input("--> Enter the amount of numbers to generate: "))
+    
+
+    if lowerLimit == upperLimit:
+        print("Please enter two different numbers!")
+        return
+    
+    random_numbers = rng_picker(lowerLimit, upperLimit, amount)
+    
+    print("=======================================================")
+    print(f"Results: {random_numbers}")
+    print("=======================================================")
+
+if __name__ == "__main__":
+    main()
